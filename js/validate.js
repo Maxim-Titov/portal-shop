9f797f145c52287b38ab159d6deacdc5add7325d
Maxim_Titov <73597552+Maxim-Titov@users.noreply.github.com>
2024-08-27 10:39:51 +0300

    Add files via upload

--- js/validate.js
+++ js/validate.js
@@ -1,26 +1,50 @@
-var name_field = $('input[name=name]');
-var phone_field = $('input[name=phone]');
-var email_field = $('input[name=email]');
+$(document).ready(function() {
+    $('#feedback-form').on('submit', function(event) {
+        var name_field = $('input[name=name]');
+        var phone_field = $('input[name=phone]');
+        var email_field = $('input[name=email]');
 
-function validate() {
+        var phonePattern = /^\+?3?8?(0\d{9})$/;
+        var emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        
+        if ($(name_field).val() === '') {
+            event.preventDefault();
 
-    if ($(name_field).is(':empty')) {
-        $(name_field).css("outline-color", "red");
-        $(name_field).css("box-shadow", "inset 0 0 10px 3px red");
-    }
+            $(name_field).addClass('error');
+        } else {
+            $(name_field).removeClass('error');
+        }
+
+        if (phone_field.val() === '') {
+            event.preventDefault();
+
+            phone_field.addClass('error');
+        } else {
+            if (!phonePattern.test(phone_field.val())) {
+                event.preventDefault();
     
-    if ($(phone_field).is(':empty')) {
-        $(phone_field).css("outline-color", "red");
-        $(phone_field).css("box-shadow", "inset 0 0 10px 3px red");
-    }
+                phone_field.addClass('error');
+            } else {
+                phone_field.removeClass('error');
+            }
+        }
+
+        if (email_field.val() === '') {
+            event.preventDefault();
+
+            email_field.addClass('error');
+        } else {
+            if (!emailPattern.test(email_field.val())) {
+                event.preventDefault();
     
-    if ($(email_field).is(':empty')) {
-        $(email_field).css("outline-color", "red");
-        $(email_field).css("box-shadow", "inset 0 0 10px 3px red");
-    }
+                email_field.addClass('error');
+            } else {
+                email_field.removeClass('error');
+            }
+        }
 
-    if ($(name_field).is(':valid') && $(phone_field).is(':valid') && $(email_field).is(':valid')) {
-        alert("Ваше повідомлення відправлено");
-    }
-
-}
+        if ($(name_field).is(':valid') && $(phone_field).is(':valid') && $(email_field).is(':valid')) {
+            alert("Ваше повідомлення відправлено");
+        }
+    });
+});
