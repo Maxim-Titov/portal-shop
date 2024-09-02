3bada77054338e5d933b571a8af83b6c2c1c669f
Maxim_Titov <73597552+Maxim-Titov@users.noreply.github.com>
2024-09-02 09:01:11 +0300

    Delete js/add_header.js

--- js/add_header.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    fetch('../html/site-elements/header.html')
-	    .then(response => response.text())
-		.then(data => {
-			document.getElementById('header').innerHTML = data;
-		});
-});
